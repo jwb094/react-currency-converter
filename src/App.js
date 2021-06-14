cae22c7982e40a64d120e883d4bcccf5cc66bb79
Josh Bello <Jb230913@live.com>
2021-06-14 19:50:23 +0100

    working in progress regarding api call and retrieve relevant data

--- src/App.js
+++ src/App.js
@@ -8,47 +8,60 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from  'axios';
 //function App() {
   class App extends Component {
     constructor(props) {
       super(props);
       this.state = {
-          current_currency: '',
+          converted_amount: '',
           desired_currency:''
       };
       this.getConvertedValue = this.getConvertedValue.bind(this);
       }
 
       getConvertedValue(){
+       
         this.setState({
-          current_currency:document.getElementById('current_currency').value,
-          desired_currency:document.getElementById('desired_currency').value
+          desired_currency:document.getElementById('desired_currency').value,
+          converted_amount:document.getElementById('amount').value
         });
-     
+        
+        axios.get(`https://v6.exchangerate-api.com/v6/${process.env.REACT_APP_KEY}/pair/GBP/${this.state.desired_currency}/${this.state.converted_amount}`)
+            .then(function (response) {
+              // handle success
+              
+              console.log(response.data.conversion_rate);
+              console.log(response.data.conversion_result);
+            })
+            .catch(function (error) {
+              console.log(error);
+            })
       }
       handleChange = (event) => {
         event.preventDefault();
-        console.log(event);
+        // console.log(event);
         this.setState({ current_currency: event.target.value });
-        console.log(this.state);
+        // console.log(this.state);
       }
 
-
+     
   render (){
+    
     return(
         <Container>
  
  <Row>
-   <Col md={{ span: 6, offset: 4 }}>{`md={{ span: 6, offset: 3 }}`}
+   <Col md={{ span: 6, offset: 4 }}>
    <Card border="primary" style={{ width: '18rem' }}>
    <Card.Header>Converter</Card.Header>
    <Card.Body>
-   <Form.Group controlId="exampleForm.ControlInput1">
+   <Form.Group>
    <Form.Label>Enter desired amount</Form.Label>
-   <Form.Control type="email" placeholder="100" />
+   <Form.Control type="text" placeholder="100"  id="amount"/>
  </Form.Group>
      <Card.Text>
-     <Form.Group >
+     {/* <Form.Group >
      <Form.Label>Select Current Currency</Form.Label>
          <Form.Control as="select" id="current_currency" onChange={this.handleChange}>
            <option value="GBP">GBP</option>
@@ -57,10 +70,10 @@ import Button from 'react-bootstrap/Button';
            <option value="JPY">JPY</option>
            <option value="HKD">HKD</option>
          </Form.Control>
-       </Form.Group>
+       </Form.Group> */}
        <Form.Group >
       <Form.Label>Select Desired Currency</Form.Label>
-         <Form.Control as="select" id="desired_currency">
+         <Form.Control as="select" id="desired_currency" onChange={this.handleChange}>
          <option value="GBP">GBP</option>
            <option value="EUR">EUR</option>
            <option value="USD">USD</option>
